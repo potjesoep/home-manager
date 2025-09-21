255a58693be89a6dc314271033fb505fd880e976
Mia Winter Schat <nekonoor@proton.me>
2025-09-21 20:04:19 +0200

    feat: add jq

--- modules/termutils.nix
+++ modules/termutils.nix
@@ -5,6 +5,7 @@
   programs.eza.enable = true;
   programs.fd.enable = true;
   programs.fzf.enable = true;
+  programs.jq.enable = true;
   programs.ripgrep.enable = true;
   programs.starship.enable = true;
   programs.zoxide.enable = true;

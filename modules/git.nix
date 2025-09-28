60ee76d4a7df027f504146e26f8d59b4d494a501
Soep <soep@tuta.io>
2025-09-28 13:26:22 +0200

    feat: update git user

--- modules/git.nix
+++ modules/git.nix
@@ -3,8 +3,8 @@
   programs.git = {
     enable = true;
     delta.enable = true;
-    userName = "Mia Winter Schat";
-    userEmail = "nekonoor@proton.me";
+    userName = "Soep";
+    userEmail = "soep@tuta.io";
     extraConfig = {
       core.editor = "nvim -c 'vsplit term://git --no-pager diff --cached | wincmd h'";
       init.defaultBranch = "main";

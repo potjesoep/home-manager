037c44094c9a1a8f546394e7d574faf3479a842b
Soep <soep@tuta.io>
2025-10-20 10:38:30 +0200

    fix: update renamed git settings

--- modules/git.nix
+++ modules/git.nix
@@ -1,11 +1,18 @@
 {
+  # git diff colors i think
+  programs.delta ={
+    enable = true;
+    enableGitIntegration = true;
+  };
+
   # enable git with my name and email
   programs.git = {
     enable = true;
-    delta.enable = true;
-    userName = "Soep";
-    userEmail = "soep@tuta.io";
-    extraConfig = {
+    settings = {
+      user = {
+        name = "Soep";
+        email = "soep@tuta.io";
+      };
       core.editor = "nvim -c 'vsplit term://git --no-pager diff --cached | wincmd h'";
       init.defaultBranch = "main";
       pull.rebase = true;
